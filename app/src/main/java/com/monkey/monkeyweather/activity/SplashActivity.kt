d550c4967816e0268312778e88d1fe1322cfeef7
Monkey <monkey@gmail.com>
2018-02-28 14:03:45 +0800

    splash

---
+++ app/src/main/java/com/monkey/monkeyweather/activity/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.monkey.monkeyweather.activity
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.support.v7.app.AppCompatActivity
+import com.monkey.monkeyweather.R
+
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        Handler().postDelayed({
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
+    }
+}
